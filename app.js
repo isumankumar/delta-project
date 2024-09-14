10ceed329c276fdcce0255befe4db1f27eb19538
Suman Kumar <sumankumar28278@gmail.com>
2024-09-14 17:59:32 +0530

    add new files

--- app.js
+++ app.js
@@ -65,9 +65,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 
 
